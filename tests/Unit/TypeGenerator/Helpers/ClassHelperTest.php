95b27e6822a09fdd2b3e4f2ca96c9dc4c35678dc
Martin Pham <i@mph.am>
2025-08-14 18:02:47 +0200

    no message

--- tests/Unit/TypeGenerator/Helpers/ClassHelperTest.php
+++ tests/Unit/TypeGenerator/Helpers/ClassHelperTest.php
@@ -3,17 +3,36 @@
 namespace Tests\Unit\TypeGenerator\Helpers;
 
 use MartinPham\TypeGenerator\Helpers\ClassHelper;
+use MartinPham\TypeGenerator\Helpers\CodeHelper;
+use MartinPham\TypeGenerator\Helpers\DocBlockHelper;
+use MartinPham\TypeGenerator\Helpers\ModelHelper;
+use MartinPham\TypeGenerator\Helpers\SchemaHelper;
 use MartinPham\TypeGenerator\Writers\OpenAPI\OpenAPI;
 use MartinPham\TypeGenerator\Definitions\Schemas\Schema;
 use MartinPham\TypeGenerator\Definitions\Schemas\StringSchema;
 use MartinPham\TypeGenerator\Definitions\Schemas\ObjectSchema;
+use MartinPham\TypeGenerator\Definitions\Schemas\RefSchema;
+use MartinPham\TypeGenerator\Definitions\Schemas\ArraySchema;
+use MartinPham\TypeGenerator\Definitions\Items\ComponentSchemaItem;
 use PHPUnit\Framework\TestCase;
 use ReflectionClass;
+use ReflectionProperty;
+use ReflectionUnionType;
 use DateTime;
 use MartinPham\TypeGenerator\Definitions\Spec;
 
 class ClassHelperTest extends TestCase
 {
+    private $specMock;
+    private $classReflectionMock;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->specMock = $this->createMock(Spec::class);
+        $this->classReflectionMock = $this->createMock(ReflectionClass::class);
+    }
 
 
     /**
@@ -21,8 +40,7 @@ class ClassHelperTest extends TestCase
      */
     public function test_parse_class_with_datetime()
     {
-        $spec = $this->createMock(OpenAPI::class);
-        $schema = ClassHelper::parseClass(DateTime::class, $spec, false);
+        $schema = ClassHelper::parseClass(DateTime::class, $this->specMock, false);
 
         $this->assertInstanceOf(StringSchema::class, $schema);
         $this->assertEquals('date-time', $schema->format);
@@ -46,11 +64,10 @@ class ClassHelperTest extends TestCase
         $tempFile = tempnam(sys_get_temp_dir(), 'test_');
         file_put_contents($tempFile, $code);
 
-        // Include the file and create a mock OpenAPI spec
+        // Include the file
         include_once $tempFile;
-        $spec = $this->createMock(OpenAPI::class);
 
-        $schema = ClassHelper::parseClass('SimpleTestClass', $spec, false);
+        $schema = ClassHelper::parseClass('SimpleTestClass', $this->specMock, false);
 
         $this->assertInstanceOf(ObjectSchema::class, $schema);
 
@@ -104,16 +121,186 @@ class ClassHelperTest extends TestCase
         $tempFile = tempnam(sys_get_temp_dir(), 'test_');
         file_put_contents($tempFile, $code);
 
-        // Include the file and create a mock spec
+        // Include the file
         include_once $tempFile;
-        $spec = $this->createMock(Spec::class);
 
-
-        $schema = ClassHelper::parseClass('ArrayPropsTestClass', $spec, false);
+        $schema = ClassHelper::parseClass('ArrayPropsTestClass', $this->specMock, false);
 
         $this->assertInstanceOf(ObjectSchema::class, $schema);
 
         // Clean up
         unlink($tempFile);
     }
+    /**
+     * Test getClassFullname with a class in the same namespace
+     */
+    public function test_get_class_fullname_same_namespace()
+    {
+        // Create a test class in the same namespace
+        $code = '<?php
+        namespace Tests\Unit\TypeGenerator\Helpers;
+
+        class TestClassInSameNamespace {}';
+
+        $tempFile = tempnam(sys_get_temp_dir(), 'test_');
+        file_put_contents($tempFile, $code);
+
+        // Include the file
+        include_once $tempFile;
+
+        // Create a reflection of the current class
+        $reflection = new ReflectionClass($this);
+
+        // Test getClassFullname
+        $fullname = ClassHelper::getClassFullname('TestClassInSameNamespace', $reflection);
+
+        $this->assertEquals('Tests\Unit\TypeGenerator\Helpers\TestClassInSameNamespace', $fullname);
+
+        // Clean up
+        unlink($tempFile);
+    }
+
+    /**
+     * Test getClassFullname with an imported class
+     */
+    public function test_get_class_fullname_imported()
+    {
+        // Create a test class with imports
+        $code = '<?php
+        namespace Tests\Unit\TypeGenerator\Helpers;
+
+        use DateTime as MyDateTime;
+
+        class TestClassWithImports {}';
+
+        $tempFile = tempnam(sys_get_temp_dir(), 'test_');
+        file_put_contents($tempFile, $code);
+
+        // Include the file
+        include_once $tempFile;
+
+        // Create a reflection of the test class
+        $reflection = new ReflectionClass('Tests\Unit\TypeGenerator\Helpers\TestClassWithImports');
+
+        // Test getClassFullname with an imported class
+        $fullname = ClassHelper::getClassFullname('MyDateTime', $reflection);
+
+        $this->assertEquals('DateTime', $fullname);
+
+        // Clean up
+        unlink($tempFile);
+    }
+
+    /**
+     * Test parseClass with class docblock properties
+     */
+    public function test_parse_class_with_docblock_properties()
+    {
+        // Create a class with docblock properties
+        $code = '<?php
+        /**
+         * @property string $docProp1 First docblock property
+         * @property int $docProp2 Second docblock property
+         */
+        class DocblockPropsTestClass {
+            // No actual properties, just docblock ones
+        }';
+
+        $tempFile = tempnam(sys_get_temp_dir(), 'test_');
+        file_put_contents($tempFile, $code);
+
+        // Include the file
+        include_once $tempFile;
+
+        $schema = ClassHelper::parseClass('DocblockPropsTestClass', $this->specMock, false);
+
+        $this->assertInstanceOf(ObjectSchema::class, $schema);
+
+        $properties = $schema->properties;
+        $this->assertCount(2, $properties);
+
+        $this->assertArrayHasKey('docProp1', $properties);
+        $this->assertEquals('string', $properties['docProp1']->type);
+
+        $this->assertArrayHasKey('docProp2', $properties);
+        $this->assertEquals('integer', $properties['docProp2']->type);
+
+        // Clean up
+        unlink($tempFile);
+    }
+
+    /**
+     * Test parseClass with union types
+     */
+    public function test_parse_class_with_union_types()
+    {
+        // Skip this test if PHP version doesn't support union types
+        if (version_compare(PHP_VERSION, '8.0.0', '<')) {
+            $this->markTestSkipped('Union types are only supported in PHP 8.0+');
+            return;
+        }
+
+        // Create a class with union type properties
+        $code = '<?php
+        class UnionTypeTestClass {
+            public string|int $unionProp;
+            public string|null $nullableUnionProp;
+        }';
+
+        $tempFile = tempnam(sys_get_temp_dir(), 'test_');
+        file_put_contents($tempFile, $code);
+
+        // Include the file
+        include_once $tempFile;
+
+        $schema = ClassHelper::parseClass('UnionTypeTestClass', $this->specMock, false);
+
+        $this->assertInstanceOf(ObjectSchema::class, $schema);
+
+        $properties = $schema->properties;
+
+        // The exact behavior will depend on how SchemaHelper::mergeSchemas works
+        // but we can at least verify the properties exist
+        $this->assertArrayHasKey('unionProp', $properties);
+        $this->assertArrayHasKey('nullableUnionProp', $properties);
+        $this->assertTrue($properties['nullableUnionProp']->nullable);
+
+        // Clean up
+        unlink($tempFile);
+    }
+
+    /**
+     * Test parseClass with onlyFromDocblock parameter
+     */
+    public function test_parse_class_only_from_docblock()
+    {
+        // Create a class with both regular properties and docblock properties
+        $code = '<?php
+        /**
+         * @property string $docProp Documentation property
+         */
+        class MixedPropsTestClass {
+            public int $regularProp;
+        }';
+
+        $tempFile = tempnam(sys_get_temp_dir(), 'test_');
+        file_put_contents($tempFile, $code);
+
+        // Include the file
+        include_once $tempFile;
+
+        // Parse with onlyFromDocblock = true
+        $schema = ClassHelper::parseClass('MixedPropsTestClass', $this->specMock, false, true);
+
+        $this->assertInstanceOf(ObjectSchema::class, $schema);
+
+        $properties = $schema->properties;
+        $this->assertCount(1, $properties);
+        $this->assertArrayHasKey('docProp', $properties);
+        $this->assertArrayNotHasKey('regularProp', $properties);
+
+        // Clean up
+        unlink($tempFile);
+    }
+
 }
---
+++ tests/Unit/TypeGenerator/Helpers/CodeHelperTest.php
@@ -0,0 +1,484 @@
+<?php
+
+namespace Tests\Unit\TypeGenerator\Helpers;
+
+use MartinPham\TypeGenerator\Helpers\CodeHelper;
+use PHPUnit\Framework\TestCase;
+use PhpParser\Node;
+use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Expr\ArrayItem;
+use PhpParser\Node\Expr\ClassConstFetch;
+use PhpParser\Node\Expr\ConstFetch;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Name;
+use PhpParser\Node\NullableType;
+use PhpParser\Node\Scalar\DNumber;
+use PhpParser\Node\Scalar\LNumber;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\Property;
+use PhpParser\NodeVisitor;
+use PhpParser\NodeVisitor\NodeVisitorAbstract;
+use ReflectionClass;
+
+class CodeHelperTest extends TestCase
+{
+    /**
+     * Test getImports method with simple imports
+     */
+    public function test_get_imports_simple()
+    {
+        $code = '<?php
+        namespace Test;
+
+        use Foo\Bar;
+        use Baz\Qux;
+
+        class TestClass {}
+        ';
+
+        $imports = CodeHelper::getImports($code);
+
+        $this->assertIsArray($imports);
+        $this->assertCount(2, $imports);
+        $this->assertEquals('Foo\Bar', $imports['Bar']);
+        $this->assertEquals('Baz\Qux', $imports['Qux']);
+    }
+
+    /**
+     * Test getImports method with aliased imports
+     */
+    public function test_get_imports_with_aliases()
+    {
+        $code = '<?php
+        namespace Test;
+
+        use Foo\Bar as Baz;
+        use Qux\Quux as Corge;
+
+        class TestClass {}
+        ';
+
+        $imports = CodeHelper::getImports($code);
+
+        $this->assertIsArray($imports);
+        $this->assertCount(2, $imports);
+        $this->assertEquals('Foo\Bar', $imports['Baz']);
+        $this->assertEquals('Qux\Quux', $imports['Corge']);
+    }
+
+    /**
+     * Test getImports method with grouped imports
+     */
+    public function test_get_imports_grouped()
+    {
+        $code = '<?php
+        namespace Test;
+
+        use Foo\Bar\{Baz, Qux as Quux};
+
+        class TestClass {}
+        ';
+
+        $imports = CodeHelper::getImports($code);
+
+        $this->assertIsArray($imports);
+        $this->assertCount(2, $imports);
+        $this->assertEquals('Foo\Bar\Baz', $imports['Baz']);
+        $this->assertEquals('Foo\Bar\Qux', $imports['Quux']);
+    }
+
+    /**
+     * Test getImports method with multi-line imports
+     */
+    public function test_get_imports_multiline()
+    {
+        $code = '<?php
+        namespace Test;
+
+        use Foo\
+            Bar;
+        use Baz\
+            Qux;
+
+        class TestClass {}
+        ';
+
+        $imports = CodeHelper::getImports($code);
+
+        $this->assertIsArray($imports);
+        $this->assertCount(2, $imports);
+        $this->assertEquals('Foo\Bar', $imports['Bar']);
+        $this->assertEquals('Baz\Qux', $imports['Qux']);
+    }
+
+    /**
+     * Test extractArgumentValue method with class constant
+     */
+    public function test_extract_argument_value_class_const()
+    {
+        $class = new Name('TestClass');
+        $name = new Identifier('class');
+        $arg = new ClassConstFetch($class, $name);
+
+        $value = CodeHelper::extractArgumentValue($arg);
+
+        $this->assertEquals('TestClass', $value);
+    }
+
+    /**
+     * Test extractArgumentValue method with string
+     */
+    public function test_extract_argument_value_string()
+    {
+        $arg = new String_('test_string');
+
+        $value = CodeHelper::extractArgumentValue($arg);
+
+        $this->assertEquals('test_string', $value);
+    }
+
+    /**
+     * Test extractArgumentValue method with number
+     */
+    public function test_extract_argument_value_number()
+    {
+        $arg = new LNumber(42);
+
+        $value = CodeHelper::extractArgumentValue($arg);
+
+        $this->assertEquals(42, $value);
+    }
+
+    /**
+     * Test extractArgumentValue method with variable
+     */
+    public function test_extract_argument_value_variable()
+    {
+        $arg = new Variable('testVar');
+
+        $value = CodeHelper::extractArgumentValue($arg);
+
+        $this->assertEquals('$testVar', $value);
+    }
+
+    /**
+     * Test extractArgumentValue method with unknown type
+     */
+    public function test_extract_argument_value_unknown()
+    {
+        $arg = new ConstFetch(new Name('TEST_CONST'));
+
+        $value = CodeHelper::extractArgumentValue($arg);
+
+        $this->assertEquals('unknown', $value);
+    }
+
+    /**
+     * Test extractStringValue method with string
+     */
+    public function test_extract_string_value()
+    {
+        $node = new String_('test_string');
+
+        $value = CodeHelper::extractStringValue($node);
+
+        $this->assertEquals('test_string', $value);
+    }
+
+    /**
+     * Test extractStringValue method with non-string
+     */
+    public function test_extract_string_value_non_string()
+    {
+        $node = new LNumber(42);
+
+        $value = CodeHelper::extractStringValue($node);
+
+        $this->assertNull($value);
+    }
+
+    /**
+     * Test extractArrayValues method with array of strings
+     */
+    public function test_extract_array_values()
+    {
+        $items = [
+            new ArrayItem(new String_('value1')),
+            new ArrayItem(new String_('value2')),
+            new ArrayItem(new String_('value3'))
+        ];
+        $node = new Array_($items);
+
+        $values = CodeHelper::extractArrayValues($node);
+
+        $this->assertIsArray($values);
+        $this->assertCount(3, $values);
+        $this->assertEquals(['value1', 'value2', 'value3'], $values);
+    }
+
+    /**
+     * Test extractArrayValues method with non-array
+     */
+    public function test_extract_array_values_non_array()
+    {
+        $node = new String_('not_an_array');
+
+        $values = CodeHelper::extractArrayValues($node);
+
+        $this->assertIsArray($values);
+        $this->assertEmpty($values);
+    }
+
+    /**
+     * Test extractAssocArrayValues method with associative array
+     */
+    public function test_extract_assoc_array_values()
+    {
+        $items = [
+            new ArrayItem(new String_('value1'), new String_('key1')),
+            new ArrayItem(new String_('value2'), new String_('key2')),
+            new ArrayItem(new LNumber(42), new String_('key3'))
+        ];
+        $node = new Array_($items);
+
+        $values = CodeHelper::extractAssocArrayValues($node);
+
+        $this->assertIsArray($values);
+        $this->assertCount(3, $values);
+        $this->assertEquals([
+            'key1' => 'value1',
+            'key2' => 'value2',
+            'key3' => 42
+        ], $values);
+    }
+
+    /**
+     * Test extractAssocArrayValues method with non-array
+     */
+    public function test_extract_assoc_array_values_non_array()
+    {
+        $node = new String_('not_an_array');
+
+        $values = CodeHelper::extractAssocArrayValues($node);
+
+        $this->assertIsArray($values);
+        $this->assertEmpty($values);
+    }
+
+    /**
+     * Test extractScalarValue method with string
+     */
+    public function test_extract_scalar_value_string()
+    {
+        $node = new String_('test_string');
+
+        $value = CodeHelper::extractScalarValue($node);
+
+        $this->assertEquals('test_string', $value);
+    }
+
+    /**
+     * Test extractScalarValue method with integer
+     */
+    public function test_extract_scalar_value_integer()
+    {
+        $node = new LNumber(42);
+
+        $value = CodeHelper::extractScalarValue($node);
+
+        $this->assertEquals(42, $value);
+    }
+
+    /**
+     * Test extractScalarValue method with float
+     */
+    public function test_extract_scalar_value_float()
+    {
+        $node = new DNumber(3.14);
+
+        $value = CodeHelper::extractScalarValue($node);
+
+        $this->assertEquals(3.14, $value);
+    }
+
+    /**
+     * Test extractScalarValue method with constant
+     */
+    public function test_extract_scalar_value_constant()
+    {
+        $node = new ConstFetch(new Name('TEST_CONST'));
+
+        $value = CodeHelper::extractScalarValue($node);
+
+        $this->assertEquals('TEST_CONST', $value);
+    }
+
+    /**
+     * Test extractScalarValue method with non-scalar
+     */
+    public function test_extract_scalar_value_non_scalar()
+    {
+        $node = new Variable('testVar');
+
+        $value = CodeHelper::extractScalarValue($node);
+
+        $this->assertNull($value);
+    }
+
+    /**
+     * Test getTypeString method with Name
+     */
+    public function test_get_type_string_name()
+    {
+        $type = new Name('TestType');
+
+        $typeString = CodeHelper::getTypeString($type);
+
+        $this->assertEquals('TestType', $typeString);
+    }
+
+    /**
+     * Test getTypeString method with Identifier
+     */
+    public function test_get_type_string_identifier()
+    {
+        $type = new Identifier('string');
+
+        $typeString = CodeHelper::getTypeString($type);
+
+        $this->assertEquals('string', $typeString);
+    }
+
+    /**
+     * Test getTypeString method with NullableType
+     */
+    public function test_get_type_string_nullable()
+    {
+        $innerType = new Identifier('string');
+        $type = new NullableType($innerType);
+
+        $typeString = CodeHelper::getTypeString($type);
+
+        $this->assertEquals('string|null', $typeString);
+    }
+
+    /**
+     * Test getTypeString method with unknown type
+     */
+    public function test_get_type_string_unknown()
+    {
+        $type = new Variable('testVar');
+
+        $typeString = CodeHelper::getTypeString($type);
+
+        $this->assertEquals('unknown', $typeString);
+    }
+
+    /**
+     * Test createAST and parseClassCode methods
+     * This test is skipped because it requires actual PHP files
+     */
+    public function test_parse_class_code()
+    {
+        $this->markTestSkipped('This test requires actual PHP files');
+
+        // Create a simple test class
+        $code = '<?php
+        namespace Tests\Unit\TypeGenerator\Helpers;
+
+        class TestParseClass {
+            public string $testProp = "test";
+
+            public function testMethod() {
+                return "test";
+            }
+        }';
+
+        $tempFile = tempnam(sys_get_temp_dir(), 'test_');
+        file_put_contents($tempFile, $code);
+
+        // Include the file
+        include_once $tempFile;
+
+        // Create a reflection of the test class
+        $reflection = new ReflectionClass('Tests\Unit\TypeGenerator\Helpers\TestParseClass');
+
+        // Create a simple visitor that collects property names
+        $visitor = new class extends NodeVisitorAbstract {
+            public $results = [];
+
+            public function leaveNode(Node $node) {
+                if ($node instanceof Property) {
+                    foreach ($node->props as $prop) {
+                        $this->results[] = $prop->name->toString();
+                    }
+                }
+            }
+        };
+
+        // Parse the class code
+        $results = CodeHelper::parseClassCode($reflection, $visitor);
+
+        $this->assertIsArray($results);
+        $this->assertContains('testProp', $results);
+
+        // Clean up
+        unlink($tempFile);
+    }
+
+    /**
+     * Test parseClassNodes method
+     * This test is skipped because it requires actual PHP files
+     */
+    public function test_parse_class_nodes()
+    {
+        $this->markTestSkipped('This test requires actual PHP files');
+
+        // Create a simple test class
+        $code = '<?php
+        namespace Tests\Unit\TypeGenerator\Helpers;
+
+        class TestNodesClass {
+            public string $testProp = "test";
+
+            public function testMethod() {
+                return "test";
+            }
+        }';
+
+        $tempFile = tempnam(sys_get_temp_dir(), 'test_');
+        file_put_contents($tempFile, $code);
+
+        // Include the file
+        include_once $tempFile;
+
+        // Create a reflection of the test class
+        $reflection = new ReflectionClass('Tests\Unit\TypeGenerator\Helpers\TestNodesClass');
+
+        $properties = [];
+        $methods = [];
+
+        // Parse the class nodes
+        CodeHelper::parseClassNodes(
+            $reflection,
+            function ($property) use (&$properties) {
+                foreach ($property->props as $prop) {
+                    $properties[] = $prop->name->toString();
+                }
+            },
+            function ($method, $returnNodes) use (&$methods) {
+                $methods[] = $method->name->toString();
+            }
+        );
+
+        $this->assertIsArray($properties);
+        $this->assertContains('testProp', $properties);
+
+        $this->assertIsArray($methods);
+        $this->assertContains('testMethod', $methods);
+
+        // Clean up
+        unlink($tempFile);
+    }
+}
